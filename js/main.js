1aa12b637b4849a0f6f5ed8a67e6678a3cf7b986
Aminah Tabassum <99741107+aminah07@users.noreply.github.com>
2023-09-15 12:40:54 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,25 @@
+function loading(){
+    document.getElementById('preloader').style.display = 'none';
+} 
+
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
